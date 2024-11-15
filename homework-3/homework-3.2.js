cba865bee701bf1f36ac8d3b7b758875ce064c18
AleksandrBida <aleksandr13b@gmail.com>
2024-11-15 15:46:24 +0200

    git homework3

---
+++ homework-3/homework-3.2.js
@@ -0,0 +1,10 @@
+const string = "String";
+const number = 1;
+const boolean = true;
+let myUndefined;
+const symbol = Symbol("example");
+const bigInt = 1n;
+const object = Object();
+// \t \n \\ \' \"
+const message4 = `JS types:\n\n${typeof string}\n${typeof number}\n${typeof boolean}\n${null}\n${typeof myUndefined}\n${typeof symbol}\n${typeof bigInt}\n${typeof object}`;
+console.log(message4);
